262a85b9fdbe48b642a1b77dccb34692ea035963
HP <mega.pe4enka@gmail.com>
2021-10-27 01:41:06 +0300

    Redux/redux thunk

--- src/index.js
+++ src/index.js
@@ -4,10 +4,30 @@ import {BrowserRouter} from 'react-router-dom'
 import './index.css'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
+import {applyMiddleware, compose, createStore} from "redux"
+import thunk from "redux-thunk"
+import {Provider} from "react-redux"
+import rootReducer from "./store/reducers/rootReducer"
+
+const composeEnhancers =
+    typeof window === 'object' &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+        }) : compose;
+
+
+const store = createStore(
+    rootReducer,
+    composeEnhancers(
+        applyMiddleware(thunk)
+    )
+)
 
 ReactDOM.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>,
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>,
     document.getElementById('root'))
 registerServiceWorker()
